ad0dec213d9cc8a11c204b6343ee7c8704e54a13
Aszan <aszan06@gmail.com>
2016-09-01 10:06:11 +0200

    ToProperty subscription issue

---
+++ Reac/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿
+using ReactiveUI;
+using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reac
+{
+
+    public interface IDuck
+    {
+        IChicken Chicken { get; }
+        void Foo();
+    }
+
+    public interface IChicken
+    {
+    }
+
+    public class Chicken : IChicken
+    {
+
+    }
+
+    public class Duck : IDuck, INotifyPropertyChanged
+    {
+        public IChicken Chicken { get; }
+
+        public Duck()
+        {
+            Foo();
+        }
+
+        public void Foo()
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(nameof(Chicken)));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+
+
+    public class MainViewModel : ReactiveObject
+    {
+        private IDuck _duck;
+        public IDuck Duck
+        {
+            get { return _duck; }
+            set { this.RaiseAndSetIfChanged(ref _duck, value); }
+        }
+
+        private readonly ObservableAsPropertyHelper<string> dog;
+        public string Dog => dog.Value;
+
+        public MainViewModel()
+        {
+            Duck = new Duck();
+
+            dog = this.WhenAnyValue(x => x.Duck.Chicken)
+			          .Select(x => Guid.NewGuid().ToString())
+                    .ToProperty(this, x => x.Dog, "dog");
+
+            this.WhenAnyValue(x => x.Duck.Chicken)
+                .Subscribe(_ => Hit());
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Duck.Foo();
+				Thread.Sleep(TimeSpan.FromSeconds(1));
+				Duck.Foo();
+            });
+        }
+
+        public void Hit()
+        {
+            Console.WriteLine(Dog);
+        }
+    }
+}
---
+++ Reac/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using System.Windows;
+using System;
+
+namespace Reac
+{
+    public partial class MainWindow : Window, IViewFor<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.WhenAnyValue(x => x.ViewModel)
+                .BindTo(this, x => x.DataContext);
+
+            ViewModel = new MainViewModel();
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel)value; }
+        }
+    }
+}
